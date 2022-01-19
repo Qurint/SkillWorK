7ab203f50f7f987d4530eb694ec95055e6d09886
Qurint <korshunov.m@yahoo.com>
2022-01-20 00:37:02 +0300

    HW_5

---
+++ Module_5_HomeWork/Work_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using static System.Console;
+
+namespace Work_1
+{
+    internal class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param Split="Ввод входных данных и их обработка"></param>
+        static void SplitText(string _text)
+        {
+            string[] _letters = _text.Split(' ');
+
+            foreach (var _letter in _letters)
+            {
+                 Conclusion(_letter);
+            }
+        }
+        static void Conclusion(string _text)
+        {
+            WriteLine($"Вывод : {_text} ");
+        }
+
+            static void Main(string[] args)
+        {
+            string _text;
+            _text = ReadLine();            
+
+            SplitText(_text);
+
+            ReadKey();
+           
+        }
+    }
+}
---
+++ Module_5_HomeWork/Work_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using static System.Console;
+
+namespace Work_2
+{
+    internal class Program
+    {
+        static void SplitText(string _text)
+        {
+            string[] _letters = _text.Split(' ');
+
+                Permutation(_letters);
+           
+        }
+
+        static void Permutation(string[] _text)
+        {
+            string[] _letters = new string[_text.Length];
+            for(int a = 0; a < _text.Length; a++)
+            {
+                _letters[a] = _text[a];
+            }
+            for(int a = _letters.Length -1; a >= 0; a--)
+            {
+                Write($"{_letters[a]} ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string _text;
+            _text = ReadLine();
+
+            SplitText(_text);
+
+            ReadKey();
+
+        }
+    }
+}
